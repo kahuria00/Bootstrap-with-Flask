40016cc215800891a87699335749a5769ce42b97
kahuria00 <charitykahuria@gmail.com>
2020-03-04 09:13:26 +0300

    web app bootstrap & flask

---
+++ app.py
@@ -0,0 +1,25 @@
+import os
+import json
+from flask import Flask, jsonify, render_template, request,redirect
+
+
+app=Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+@app.route('/button', methods=['POST','GET'])
+def button():
+    if request.method=='POST':
+        startdate=request.form['startdate']
+        enddate=request.form['enddate']
+        return render_template('button.html',startdate=startdate,enddate=enddate)
+        print(startdate,enddate)
+
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+   app.run(debug=True)
